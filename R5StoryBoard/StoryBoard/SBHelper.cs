18892839b4b7f0521b9ce9599e2da4250a1ebd5b
pumhatre <pumhatre@deloitte.com>
2014-10-10 14:48:33 +0530

    xyz
    checkin

---
+++ R5StoryBoard/StoryBoard/SBHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace StoryBoard
+{
+    public class SBHelper
+    {
+        public static string DataTableToJSON(DataTable dtDataTable)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row = null;
+
+            foreach (DataRow dr in dtDataTable.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dtDataTable.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+            return serializer.Serialize(rows);
+        }
+
+        public static string EncodeData(string strData)
+        {
+            return HttpUtility.HtmlEncode(strData).Replace(Environment.NewLine, "<br>");;
+        }
+
+        public static string DecodeData(string strData)
+        {
+            return HttpUtility.HtmlDecode(strData).Replace("<br>", Environment.NewLine);
+        }
+    }
+}
---
+++ R5StoryBoard/StoryBoard/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoryBoard
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public byte RoleId { get; set; }
+        public User()
+        {
+
+        }
+        public User(int userid,string username,string fullname,byte role)
+        {
+            this.UserID = userid;
+            this.UserName = username;
+            this.FullName = fullname;
+            this.RoleId = role;
+        }
+    }
+}
